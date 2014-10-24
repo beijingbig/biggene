2ca630074190ead98c5620b71d0d934a0a0497d8
beijingbig <beijingbig@outlook.com>
2014-10-24 09:12:49 +0800

    c1

---
+++ xiangmu_test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace xiangmu_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n\t开始测试");
+            MessageBox.Show("Hello World!", "Message from C#");
+            Console.ReadKey();
+        }
+    }
+}
